976ef45acbf04347aa4eb60b9559b442d14c7c71
Jerry <lujing2457@163.com>
2024-08-14 14:20:25 +0800

    feat: update config

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 const config = {
-  printWidth: 800, // 每行代码的最大宽度
-  semi: true, // 每行代码结尾加分号
-  singleQuote: true, // 单引号
-  trailingComma: 'all', // 尽可能使用尾随逗号
+  // printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
   endOfLine: 'auto',
 };
 
