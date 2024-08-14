78780c6d1e4d68099f2b649507fa8f5a5a920759
Jerry <lujing2457@163.com>
2024-08-14 14:57:13 +0800

    feat: add URL 参数转换为一个对象

--- packages/url/src/index.ts
+++ packages/url/src/index.ts
@@ -1,10 +1,11 @@
 import { isNull } from '@buck-shared/is';
+
 /**
  * 获取当前url参数
  * @param name
  * @returns value
  */
-export function getQueryString(name: string): string | null {
+export const getQueryString = (name: string): string | null => {
   const reg = new RegExp(
     '(^|&)' + encodeURIComponent(name) + '=([^&]*)(&|$)',
     'i',
@@ -13,4 +14,21 @@ export function getQueryString(name: string): string | null {
     .substring(1)
     .match(reg);
   return isNull(result) ? null : decodeURIComponent(result[2]);
-}
+};
+
+/**
+ * URL 参数转换为一个对象
+ * @param url
+ * @returns
+ */
+export const parseUrlParamsToObj = (url: string): Record<string, string> => {
+  const urlStr = url ? url : window.location.href;
+  const searchParams = new URLSearchParams(urlStr.split('?')[1]);
+  const paramsObj: Record<string, string> = {};
+
+  for (const [key, value] of searchParams.entries()) {
+    paramsObj[key] = value;
+  }
+
+  return paramsObj;
+};
